43ac8e4fb097f33bded7eb4f6d18b9370c88e9fc
JASMINE KOH <runawaykid@gmail.com>
2015-11-23 16:55:28 +0800

    First commit

---
+++ mapIt.py
@@ -0,0 +1,12 @@
+# mapIt.py
+# Open map of address in browser
+
+import webbrowser, sys, pyperclip
+if len(sys.argv) > 1:
+	address = ' '.join(sys.argv[1:])
+else:
+	address = pyperclip.paste()
+
+print address
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
